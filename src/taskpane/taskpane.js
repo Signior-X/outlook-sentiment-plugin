412bf46a9d6cb79ab28683f7bdc853ae3c212adf
Priyam Seth <priyamseth@microsoft.com>
2024-09-20 15:27:06 +0530

    Fix task pane 2 and my current work

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,19 +7,58 @@
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    // document.getElementById("sideload-msg").style.display = "none";
+    // document.getElementById("app-body").style.display = "flex";
 
-    // Register an event handler to identify when messages are selected.
-    Office.context.mailbox.addHandlerAsync(Office.EventType.SelectedItemsChanged, run, (asyncResult) => {
-      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.log(asyncResult.error.message);
-        return;
+    const dummyCardsData = [
+      {
+        imgSrc: "../../assets/logo-filled.png",
+        senderName: "Abhinav Gandotra",
+        emailTitle: "Meeting regarding finalization of work demo.",
+        emailDescription: "Here we will give a quick summary of what have been done! This will help the reader a glimpse about the thread.",
+      },
+      {
+        imgSrc: "../../assets/logo-filled.png",
+        senderName: "Jane Doe",
+        emailTitle: "Meeting regarding finalization of work demo.",
+        emailDescription: "Here we will give a quick summary of what have been done! This will help the reader a glimpse about the thread.",
+      },
+      {
+        imgSrc: "../../assets/logo-filled.png",
+        senderName: "John Doe",
+        emailTitle: "Meeting regarding finalization of work demo.",
+        emailDescription: "Here we will give a quick summary of what have been done! This will help the reader a glimpse about the thread.",
+      },
+      {
+        imgSrc: "../../assets/logo-filled.png",
+        senderName: "Jane Doe",
+        emailTitle: "Meeting regarding finalization of work demo.",
+        emailDescription: "Here we will give a quick summary of what have been done! This will help the reader a glimpse about the thread.",
       }
+    ]
 
-      console.log("Event handler added.");
+
+    let cardsHtml = "";
+    dummyCardsData.forEach((card) => {
+      cardsHtml += `<fluent-card class="card">
+                <div class="card-icon">
+                    <img src="https://outlook.office365.com/10bb6ab6-495b-4322-890d-71c846ad427e" alt="Email Icon" />
+                </div>
+                <div class="card-content">
+                    <fluent-card-header>
+                        <fluent-card-header-title class="sender-name">${card.senderName}</fluent-card-header-title>
+                    </fluent-card-header>
+                    <fluent-card-body>
+                        <p class="email-title">Meeting regarding finalization of work demo.</p>
+                        <p class="email-description">Here we will give a quick summary of what have been done! This will help the reader a glimpse about the thread.</p>
+                        <fluent-badge appearance="accent">New</fluent-badge>
+                    </fluent-card-body>
+                </div>
+            </fluent-card>`;
     });
+
+    const cards = document.getElementById("cards");
+    cards.innerHTML = cardsHtml;
   }
 });
 
@@ -27,24 +66,5 @@ export async function run() {
   /**
    * Insert your Outlook code here
    */
-
-  // Clear list of previously selected messages, if any.
-  const list = document.getElementById("selected-items");
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
-
-  // Retrieve the subject line of the selected messages and log it to a list in the task pane.
-  Office.context.mailbox.getSelectedItemsAsync((asyncResult) => {
-    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-      console.log(asyncResult.error.message);
-      return;
-    }
-
-    asyncResult.value.forEach((item) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = item.subject;
-      list.appendChild(listItem);
-    });
-  });
+  // Here we can write any logic if we want
 }
